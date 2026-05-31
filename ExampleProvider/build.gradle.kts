c6fe149bccb00376493a91408615de2d34eb2b20
VickyGittub <vicioustrodoon@gmail.com>
2026-05-31 10:13:17 +0530

    Change cloudstream3 dependency to pre-release version

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     // Add cloudstream3 dependency
-    implementation("com.lagradost:cloudstream3:latest.release")
+    implementation("com.lagradost:cloudstream3:pre-release")
 }
 
 // Use an integer for version numbers
