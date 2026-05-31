d348efa9546c98a675480c1e9dc9cc93dd2c059a
VickyGittub <vicioustrodoon@gmail.com>
2026-05-31 14:49:57 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,10 @@ allprojects {
         maven("https://jitpack.io")
     }
 }
-
+plugins {
+    id "kotlin-android"
+    id "org.jetbrains.kotlin.plugin.serialization" version "1.9.20" // or your Kotlin version
+}
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
 fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
@@ -86,6 +89,7 @@ subprojects {
         implementation(kotlin("stdlib")) // Adds Standard Kotlin Features
         implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP Lib
         implementation("org.jsoup:jsoup:1.18.3") // HTML Parser
+        implementation "org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2"
         // IMPORTANT: Do not bump Jackson above 2.13.1, as newer versions will
         // break compatibility on older Android devices.
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") // JSON Parser
