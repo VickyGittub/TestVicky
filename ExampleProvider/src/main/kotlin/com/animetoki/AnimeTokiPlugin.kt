52ada3951e94e00fe52e5e740fc8026241d14dec
VickyGittub <vicioustrodoon@gmail.com>
2026-05-31 14:33:35 +0530

    Add empty parentheses to AnimeTokiPlugin class

--- ExampleProvider/src/main/kotlin/com/animetoki/AnimeTokiPlugin.kt
+++ ExampleProvider/src/main/kotlin/com/animetoki/AnimeTokiPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class AnimeTokiPlugin : Plugin {
+class AnimeTokiPlugin : Plugin() {  // Add empty parentheses
     override fun load(context: Context) {
         registerMainAPI(AnimeTokiProvider())
     }
