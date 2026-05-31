8fc601383ac03b5f60dd7ad1ab6e5f27cebc9cc8
VickyGittub <vicioustrodoon@gmail.com>
2026-05-31 10:04:24 +0530

    Add cloudstream3 dependency to build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,6 +1,8 @@
 dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    // Add cloudstream3 dependency
+    implementation("com.lagradost:cloudstream3:latest.release")
 }
 
 // Use an integer for version numbers
