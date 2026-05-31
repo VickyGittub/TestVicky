ea4002b4c3c0249b38fa3252b914ffb4d4590fa2
VickyGittub <vicioustrodoon@gmail.com>
2026-05-31 11:27:07 +0530

    Replace cloudstream3 dependency with recloudstream

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -2,7 +2,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     // Add cloudstream3 dependency
-    implementation("com.lagradost:cloudstream3:pre-release")
+    compileOnly("com.github.recloudstream:cloudstream:master-SNAPSHOT")
+    implementation("org.jsoup:jsoup:1.17.2")
 }
 
 // Use an integer for version numbers
